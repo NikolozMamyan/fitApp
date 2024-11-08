b0d449a8e4a2488d9dd03b094511086305bac2bc
Nikoloz <nikoloz.mamyan@lnstrade.fr>
2024-11-08 14:52:31 +0100

    activity

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -11,8 +11,27 @@ class ActivityController extends AbstractController
     #[Route('/activity', name: 'app_activity')]
     public function index(): Response
     {
+        $exercises = [
+            [
+                'title' => 'Squats',
+                'description' => 'Faites 3 séries de 10 squats.',
+                'image' => '/images/squat.jpg',
+            ],
+            [
+                'title' => 'Planches',
+                'description' => 'Tenez la planche pendant 30 secondes.',
+                'image' => '/images/planche.jpg',
+            ],
+            [
+                'title' => 'Burpees',
+                'description' => 'Faites 15 burpees.',
+                'image' => '/images/burpees.jpg',
+            ],
+        ];
+
         return $this->render('activity/index.html.twig', [
             'controller_name' => 'ActivityController',
+            'exercises' => $exercises,
         ]);
     }
 }
